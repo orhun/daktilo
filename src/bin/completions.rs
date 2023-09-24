56972fa63dcb2e692c03b97f46245db26491abac
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-09-24 17:56:51 +0300

    feat(cli): add shell completions and manpage generator

---
+++ src/bin/completions.rs
@@ -0,0 +1,32 @@
+use clap::{CommandFactory, ValueEnum};
+use clap_complete::Shell;
+use daktilo::args::Args;
+use std::env;
+use std::io::Result;
+
+/// Shell completions can be created with:
+///
+/// ```sh
+/// cargo run --bin daktilo-completions
+/// ```
+///
+/// in a directory specified by the environment variable OUT_DIR.
+/// See <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
+fn main() -> Result<()> {
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR is not set");
+    let mut app = Args::command();
+    for &shell in Shell::value_variants() {
+        clap_complete::generate_to(shell, &mut app, env!("CARGO_PKG_NAME"), &out_dir)?;
+    }
+    println!("Completion scripts are generated in {out_dir:?}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn generate_completions() -> Result<()> {
+        main()
+    }
+}
---
+++ src/bin/mangen.rs
@@ -0,0 +1,36 @@
+use clap::CommandFactory;
+use clap_mangen::Man;
+use daktilo::args::Args;
+use std::env;
+use std::fs;
+use std::io::Result;
+use std::path::PathBuf;
+
+/// Man page can be created with:
+///
+/// ```sh
+/// cargo run --bin daktilo-mangen
+/// ````
+///
+/// in a directory specified by the environment variable OUT_DIR.
+/// See <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
+fn main() -> Result<()> {
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR is not set");
+    let out_path = PathBuf::from(out_dir).join(format!("{}.1", env!("CARGO_PKG_NAME")));
+    let app = Args::command();
+    let man = Man::new(app);
+    let mut buffer = Vec::<u8>::new();
+    man.render(&mut buffer)?;
+    fs::write(&out_path, buffer)?;
+    println!("Man page is generated at {out_path:?}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn generate_manpage() -> Result<()> {
+        main()
+    }
+}
